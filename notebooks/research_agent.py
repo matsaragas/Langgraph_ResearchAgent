3f7da3793ced369f757c7c0bb2d36efe439242ce
petros-pavlosypsilantis <pet_yps@hotmail.com>
2025-05-25 23:19:00 +0100

    research agent set up

--- notebooks/research_agent.py
+++ notebooks/research_agent.py
@@ -83,6 +83,7 @@ for i in tqdm(range(0, len(data), batch_size)):
 
 from typing import TypedDict, Annotated, List, Union
 from langchain_core.agents import AgentAction, AgentFinish
+from langchain_core.messages import BaseMessage
 import operator
 
 
@@ -387,3 +388,91 @@ def router(state: list):
         return "final_answer"
 
 
+tool_str_to_funct = {
+    "rag_search_filter": rag_search_filter,
+    "rag_search": rag_search,
+    "fetch_arxiv": fetch_arxiv,
+    "web_search": web_search,
+    "final_answer": final_answer
+}
+
+def run_tool(state: list):
+    tool_name = state["intermediate_steps"][-1].tool
+    tool_args = state["intermediate_steps"][-1].tool_input
+    print(f"{tool_name}.invoke(input={tool_args})")
+    # run tool
+    out = tool_str_to_funct[tool_name].invoke(input=tool_args)
+    action_out = AgentAction(
+        tool=tool_name,
+        tool_input=tool_args,
+        log=str(out)
+    )
+
+# Define Graph
+from langgraph.graph import StateGraph, END
+graph = StateGraph(AgentState)
+
+graph.add_node("oracle", run_oracle)
+graph.add_node("rag_search_filter", run_tool)
+graph.add_node("rag_search", run_tool)
+graph.add_node("fetch_arxiv", run_tool)
+graph.add_node("web_search", run_tool)
+graph.add_node("final_answer", run_tool)
+
+graph.set_entry_point("oracle")
+
+graph.add_conditional_edges(
+    source="oracle", # where in graph to start
+    path=router, # function to determine which node is called
+)
+
+# creates edges from each tool back to the oracle.
+for tool_obj in tools:
+    if tool_obj.name != "final_answer":
+        graph.add_edge(tool_obj.name, "oracle")
+
+graph.add_edge("final_answer", END)
+runnable = graph.compile()
+
+
+def build_report(output: dict):
+    research_steps = output["research_steps"]
+    if type(research_steps) is list:
+        research_steps = "\n".join([f"- {r}" for r in research_steps])
+    sources = output["sources"]
+    if type(sources) is list:
+        sources = "\n".join([f"- {s}" for s in sources])
+    return f"""
+INTRODUCTION
+----------------
+{output["introduction"]}
+
+RESEARCH STEPS
+------------------
+{research_steps}
+
+REPORT
+---------------
+{output["main_body"]}
+
+CONCLUSION
+---------------
+{output["conclusion"]}
+
+SOURCES
+---------------
+{sources}
+"""
+
+print(build_report(
+    output=out["intermediate_steps"][-1].tool_input
+))
+
+# Run an AI Example Question
+
+out = runnable.invoke({
+    "input": "tell me about AI"
+})
+print(build_report(
+    output=out["intermediate_steps"][-1].tool_input
+))
