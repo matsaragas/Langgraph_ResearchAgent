d8cfd9f38f21d712e2ca527c7a3b361b3d731a56
Petros Ypsilantis <pet_yps@hotmail.com>
2025-10-03 17:25:16 +0100

    move files to enable langsmith

---
+++ agent_research/app/src/agent/settings.py
@@ -0,0 +1,2 @@
+config = {'pinecone_api_key': "pcsk_H2epd_TG1DQBuiFhb4wDQR9RmLv4QKLXmiHADH86QHfx5uJGCF52kvPMs1YRL6AEb8fEb",
+          'serpapi_key': "6cfc13f226caec204b72a2b9db003910ea713a424377fd58da98c13a9e94e81f"}
